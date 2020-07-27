bf3e2b9f77658c15a656ea9070a93a14a69037a1
viethoanggg <49758848+viethoanggg@users.noreply.github.com>
2020-07-27 13:21:33 +0700

    add some thing

--- src/app/src/main/java/com/example/staffmanagement/View/Ultils/GeneralFunc.java
+++ src/app/src/main/java/com/example/staffmanagement/View/Ultils/GeneralFunc.java
@@ -39,5 +39,4 @@ public class GeneralFunc {
         }
         return false;
     }
-
 }
--- src/app/src/main/java/com/example/staffmanagement/View/Ultils/ImageHandler.java
+++ src/app/src/main/java/com/example/staffmanagement/View/Ultils/ImageHandler.java
@@ -1,4 +1,39 @@
 package com.example.staffmanagement.View.Ultils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
 public class ImageHandler {
+
+    public static void loadImageFromBytes(Context context, byte[] bytesImage, ImageView imageView) {
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytesImage, 0, bytesImage.length);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            ((Activity) context).getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
+            imageView.setMinimumHeight(displayMetrics.heightPixels);
+            imageView.setMinimumWidth(displayMetrics.widthPixels);
+        }
+        imageView.setImageBitmap(bitmap);
+    }
+
+    public static Bitmap getBitmapFromUriAndShowImage(Context context, Uri imageUri,ImageView imageView){
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), imageUri);
+            if (imageView != null)
+                imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
 }
