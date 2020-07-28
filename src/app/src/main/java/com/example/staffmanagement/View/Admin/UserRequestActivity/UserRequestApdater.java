5c6c14a1e9d9312c9bc830b12a1875d6d411eac5
Nguyễn Hùng Vương <47838046+Vuongnguyen1705@users.noreply.github.com>
2020-07-28 16:17:11 +0700

    vvvvvv

--- src/app/src/main/java/com/example/staffmanagement/View/Admin/UserRequestActivity/UserRequestApdater.java
+++ src/app/src/main/java/com/example/staffmanagement/View/Admin/UserRequestActivity/UserRequestApdater.java
@@ -1,7 +1,11 @@
 package com.example.staffmanagement.View.Admin.UserRequestActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.staffmanagement.Model.Database.Entity.Request;
@@ -18,6 +24,8 @@ import com.example.staffmanagement.Model.Database.Entity.StateRequest;
 import com.example.staffmanagement.Model.Database.Entity.User;
 import com.example.staffmanagement.Presenter.Admin.UserRequestPresenter;
 import com.example.staffmanagement.R;
+import com.example.staffmanagement.View.Admin.DetailRequestUser.DetailRequestUserActivity;
+import com.example.staffmanagement.View.Ultils.Constant;
 import com.example.staffmanagement.View.Ultils.GeneralFunc;
 
 import java.util.ArrayList;
@@ -41,21 +49,62 @@ public class UserRequestApdater extends RecyclerView.Adapter<UserRequestApdater.
     @Override
     public UserRequestApdater.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View itemView = layoutInflater.inflate(R.layout.item_user_request,parent,false);
+        View itemView = layoutInflater.inflate(R.layout.item_user_request, parent, false);
         return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull final UserRequestApdater.ViewHolder holder, final int position) {
-        holder.txtName.setText(mPresenter.getFullNameById(requestArrayList.get(position).getIdUser()));
+        final String fullName = mPresenter.getFullNameById(requestArrayList.get(position).getIdUser());
+        holder.txtName.setText(fullName);
         holder.txtTitle.setText(mPresenter.getTitleById(requestArrayList.get(position).getId()));
 
         holder.txtDateTime.setText(GeneralFunc.convertMilliSecToDateString(mPresenter.getDateTimeById(requestArrayList.get(position).getId())));
         addRequestState();
-        adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1,arrayListRequestState);
-        holder.getSpnRequestState().setAdapter(adapter);
-        final int idState=mPresenter.getIdStateById(requestArrayList.get(position).getId());
+        adapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, arrayListRequestState) {
+            @Override
+            public boolean isEnabled(int position) {
+                if (position == 0)
+                    return false;
+                else return true;
+            }
 
+            @Override
+            public int getItemViewType(int position) {
+
+                return super.getItemViewType(position);
+            }
+
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                if (position == 0) {
+                    textView.setTextColor(Color.parseColor("#bcbcbb"));
+                }
+                return view;
+            }
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView textView = (TextView) view;
+                if(textView.getText().equals("Waiting")){
+                    textView.setTextColor(mContext.getResources().getColor(R.color.colorWaiting));
+                }else{
+                    if (textView.getText().equals("Accept")){
+                        textView.setTextColor(mContext.getResources().getColor(R.color.colorAccept));
+                    }else {
+                        textView.setTextColor(mContext.getResources().getColor(R.color.colorDecline));
+                    }
+                }
+                return view;
+            }
+        };
+        holder.getSpnRequestState().setAdapter(adapter);
+
+        final int idState = mPresenter.getIdStateById(requestArrayList.get(position).getId());
         holder.getSpnRequestState().setSelection(getIdStateById(idState));
         holder.getSpnRequestState().setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -68,24 +117,37 @@ public class UserRequestApdater extends RecyclerView.Adapter<UserRequestApdater.
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
+
+            }
+        });
+        holder.getView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, DetailRequestUserActivity.class);
+                intent.putExtra(Constant.REQUEST_DATA_INTENT, requestArrayList.get(position));
+                intent.putExtra(Constant.STATE_NAME_INTENT, String.valueOf(holder.getSpnRequestState().getSelectedItem()));
+                intent.putExtra(Constant.FULL_NAME,fullName);
+                mContext.startActivity(intent);
             }
         });
     }
 
-    private int getIdStateById(int idRequest){
-        for(int i=0;i<stateRequestArrayList.size();i++){
-            if(stateRequestArrayList.get(i).getId()==(idRequest))
+    private int getIdStateById(int idRequest) {
+        for (int i = 0; i < stateRequestArrayList.size(); i++) {
+            if (stateRequestArrayList.get(i).getId() == (idRequest))
                 return i;
         }
         return -1;
     }
-    private int getIdStateByName(String name){
-        for(int i=0;i<stateRequestArrayList.size();i++){
-            if(stateRequestArrayList.get(i).getName().equals(name))
+
+    private int getIdStateByName(String name) {
+        for (int i = 0; i < stateRequestArrayList.size(); i++) {
+            if (stateRequestArrayList.get(i).getName().equals(name))
                 return stateRequestArrayList.get(i).getId();
         }
         return -1;
     }
+
     @Override
     public int getItemCount() {
         return requestArrayList.size();
@@ -94,25 +156,37 @@ public class UserRequestApdater extends RecyclerView.Adapter<UserRequestApdater.
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView txtName, txtTitle, txtDateTime;
         private Spinner spnRequestState;
+        private View view;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            txtName=itemView.findViewById(R.id.textViewStaffName);
-            txtTitle=itemView.findViewById(R.id.textViewRequestName);
-            txtDateTime=itemView.findViewById(R.id.textViewRequestDateTime);
-            spnRequestState=itemView.findViewById(R.id.spinnerRequestState);
+            view = itemView;
+            txtName = itemView.findViewById(R.id.textViewStaffName);
+            txtTitle = itemView.findViewById(R.id.textViewRequestName);
+            txtDateTime = itemView.findViewById(R.id.textViewRequestDateTime);
+            spnRequestState = itemView.findViewById(R.id.spinnerRequestState);
         }
+
+        public View getView() {
+            return view;
+        }
+
+        public void setView(View view) {
+            this.view = view;
+        }
+
         public Spinner getSpnRequestState() {
             return spnRequestState;
         }
 
     }
 
-    private void addRequestState(){
+    private void addRequestState() {
         // StateRequestDbHandler db = new StateRequestDbHandler(mContext);
         arrayListRequestState = new ArrayList<>();
         stateRequestArrayList = new ArrayList<>();
         stateRequestArrayList.addAll(mPresenter.getAllStateRequest());
-        for(int i = 0; i < stateRequestArrayList.size(); i++){
+        for (int i = 0; i < stateRequestArrayList.size(); i++) {
             arrayListRequestState.add(stateRequestArrayList.get(i).getName());
         }
     }
--- src/app/src/main/java/com/example/staffmanagement/View/Ultils/Constant.java
+++ src/app/src/main/java/com/example/staffmanagement/View/Ultils/Constant.java
@@ -10,4 +10,7 @@ public class Constant {
     public static final String SHARED_PREFERENCE_PASSWORD = "password";
     public static final String SHARED_PREFERENCE_REMEMBER = "remember";
     public static final String SHARED_PREFERENCE_IS_CHANGE_PROFILE = "isChangeProfile";
+    public static final String STATE_NAME_INTENT = "STATE_NAME_INTENT";
+    public static final String FULL_NAME = "FULL_NAME";
+    public static int FLAG_INTENT = 0;
 }
